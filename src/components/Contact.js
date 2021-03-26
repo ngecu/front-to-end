8e3910c548170a0845649f79a1d400e6dd6771ef
ngecu <devngecu@gmail.com>
2021-03-26 23:56:58 +0300

    changing state using event Handler

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,15 +3,30 @@ import './contact.css'
 
  class Contact extends Component {
 
+    state = {
+        showContactInfo:false
+    };
+
+    onShowClick=(e)=>{
+        this.setState({showContactInfo:!this.state.showContactInfo})
+    }
     render() {
         const {name,email,phone} = this.props.contact;
+        const {showContactInfo} = this.state;
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
-                <ul className="list-group">
+                <h4>{name} 
+                <i 
+                    onClick={()=>{
+                        this.setState({showContactInfo:!this.state.showContactInfo})
+                    }} 
+                className="fas fa-sort-down"></i> </h4>
+                {showContactInfo ? (  
+                     <ul className="list-group">
         <li className="list-group-item">Email : {email}</li>
                     <li className="list-group-item">Phone : {phone}</li>
-                </ul>
+                </ul>) : null }
+             
             </div>
         )
     }
