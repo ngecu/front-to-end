477e2e3887238239b43fa1caf75b958cf364678e
ngecu <devngecu@gmail.com>
2021-04-16 10:51:29 +0300

    created a form with state

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+class AddContacts extends Component {
+    state = {
+        name: '',
+        email: '',
+        phone:''
+    }
+    
+    render() {
+        const {name,email,phone} = this.state
+        return (
+            <div>
+               <div className="card mb-3">
+                   <div className="card-header">Add Contact</div>
+                   <div className="card-body">
+                       <form>
+                           <div className="form-group">
+                               <label htmlFor="name">Name</label>
+                               <input 
+                               type="text" 
+                               name="name" 
+                               className="form-control form-control-lg" 
+                               placeholder="Enter Name..."
+                               value = {name} 
+                               id=""/>
+                           </div>
+
+                           <div className="form-group">
+                               <label htmlFor="email">Email</label>
+                               <input 
+                               type="email" 
+                               name="email" 
+                               className="form-control form-control-lg" 
+                               placeholder="Enter Email..." 
+                               value = {email}
+                               id=""/>
+                           </div>
+
+                           <div className="form-group">
+                               <label htmlFor="phone">Phone</label>
+                               <input 
+                               type="text" 
+                               name="phone" 
+                               className="form-control form-control-lg" 
+                               placeholder="Enter Phone..." 
+                               value = {phone}
+                               id=""/>
+                           </div>
+                           <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
+                       </form>
+                   </div>
+               </div>
+            </div>
+        )
+    }
+}
+export default AddContacts;
