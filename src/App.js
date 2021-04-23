29ac4d64bfc06e5d8f2944bbe2efbcc8e701caf3
ngecu <devngecu@gmail.com>
2021-04-23 20:48:12 +0300

    started from forms again

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import './App.css';
-import Contacts from './components/contacts/Contacts';
-import Header from './components/layout/Header';
-import AddContact from './components/contacts/AddContact'
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+
 import {Provider} from './Context'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,7 +15,6 @@ export default class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <AddContact/>
         <Contacts />
         </div>
       </div>
